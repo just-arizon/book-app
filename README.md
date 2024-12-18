f95e814d05230700de2e39b8656b0da00b9f191d
Arinze Chinweuba Calistus <calistusarinzechinweuba@gmail.com>
2024-12-18 08:52:48 +0100

    new book commit

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-# book-app
-=======
 This book application is React based practice solution of understanding how props work in real-life applications. 
->>>>>>> 5d653a2 (Initial commit)
--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,42 @@
-import { useState } from "react";
-import "./App.css";
+import React from 'react'
 import Book from "./Book";
 import MasteringReact from "./assets/mastering react.svg";
 import PracticalReact from "./assets/practical react.svg";
 import ReactInAction from "./assets/react in action.svg";
-// import './styles.css';
-
-function App() {
+import ReactIsLibrary from "./assets/react is a library.svg";
+import "./App.css";
+const App = () => {
   const books = [
     {title: 'Mastering React', author: 'Anthony Pham', cover: MasteringReact},
     {title: 'Practical React', author: 'Alex Johnson', cover: PracticalReact},
-    {title: 'React in Action', author: 'Bob Climo', cover: ReactInAction}
+    {title: 'React in Action', author: 'Bob Climo', cover: ReactInAction},
+    {title: 'React is a Library', author: '  Nathan Sanford', cover: ReactIsLibrary}
+  
   ];
+
   const renderedNames = books.map((book) => {
-        return (
+    return (
 
-              <Book
-              key={crypto.randomUUID()}
-              title={book.title}
-              author={book.author}
-              cover={book.cover}
-              />
+          <Book
+          key={crypto.randomUUID()}
+          title={book.title}
+          author={book.author}
+          cover={book.cover}
+          />
 
-        )
-  });
+    )
+});
+console.log(renderedNames);
 
   return (
-    <main>
+     <main>
         <h1>Favorite Books</h1>
 
       <div className="book-list">
        {renderedNames}
       </div>
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
